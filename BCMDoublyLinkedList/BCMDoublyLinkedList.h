545ad0a9226e069120bab0395c5d02f2fedcd864
bencmorrison <(LMC149bde*)>
2016-04-22 19:22:43 +1000

    Initial Commit

---
+++ BCMDoublyLinkedList/BCMDoublyLinkedList.h
@@ -0,0 +1,19 @@
+//
+//  BCMDoublyLinkedList.h
+//  BCMDoublyLinkedList
+//
+//  Created by Ben Morrison on 22/04/2016.
+//  Copyright © 2016 Benjamin C Morrison. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BCMDoublyLinkedList.
+FOUNDATION_EXPORT double BCMDoublyLinkedListVersionNumber;
+
+//! Project version string for BCMDoublyLinkedList.
+FOUNDATION_EXPORT const unsigned char BCMDoublyLinkedListVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BCMDoublyLinkedList/PublicHeader.h>
+
+
